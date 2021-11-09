974a93a286f55389ba4c3de68a409806641020d8
liulongdu <liulongdu@163.com>
2021-11-10 00:40:02 +0800

    charactor 1110

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ class Conversion:
         self.background = []
         self.bgm = []
         self.standing = []
+        self.ch_change = {'贝拉': 'Bella', '向晚': 'Ava', '珈乐': 'Carol', '嘉然': 'Diana', '乃琳': 'Queen', '阿草': 'Acao', '男人': 'Man', '女人': 'Women', '男孩': 'Boy', '女孩': 'Girl'}
+        self.cloth_change = {'常服':'causal','舞蹈服':'dance','团服':'team','画家':'draw'};
+        self.apperence_change = {'通常':'causal','生气':'angry','微笑':'smile','惊讶':'surprised','失望':'disappointed'}
+        #self.diana_cloth = {'常服':'causal','画家':'draw','团服':'team'}；
+
+    @staticmethod
+    def character(line: str, span: tuple[int, int]) -> str:
+        """将标题行转换为提前代码块"""
+        ch_change = {'贝拉': 'Bella', '向晚': 'Ava', '珈乐': 'Carol', '嘉然': 'Diana', '乃琳': 'Queen', '阿草': 'Acao', '男人': 'Man', '女人': 'Women', '男孩': 'Boy', '女孩': 'Girl'}
+        result = "@<|\n" \
+                 "label('ch%s', '%s')\n" \
+                 "is_default_start()\n" \
+                 "|>\n" \
+                 % (cn_num[line[span[0] + 1: span[1] - 1]], line)
+        return result
 
     @staticmethod
     def head(line: str, span: tuple[int, int]) -> str:
@@ -20,12 +35,14 @@ class Conversion:
                  % (cn_num[line[span[0] + 1: span[1] - 1]], line)
         return result
 
+
     def main(self):
         """主程序"""
         line = self.original.readline()
         while line:
             if line == '\n':  # 删除空行
                 line = self.original.readline()
+                self.result.write(line)
                 continue
             line = line.strip('\n')  # 删除末尾的换行符
             if re.match('第(.*?)章', line):  # 匹配文件开头的章节名
@@ -33,9 +50,17 @@ class Conversion:
                 self.result.write(self.head(line, span))
                 line = self.original.readline()
                 continue
-            if re.match('【(.*?)】', line):  # 匹配场景配置
+            if re.match('【立绘：无立绘】', line):  # 匹配场景配置
                 line = self.original.readline()
                 continue
+            else : 
+            	if re.match('【立绘：(.*?)-(.*?)-(.*?)】', line):  # 匹配场景配置
+            		listofcharac = re.findall('【立绘：(.*?)-(.*?)-(.*?)】', line);
+            		#print(self.ch_change['贝拉']);
+            		#print(self.ch_change[listofcharac[0][0]],self.cloth_change[listofcharac[0][1]] + '_' + self.apperence_change[listofcharac[0][2]]);
+            		print('show(' + self.ch_change[listofcharac[0][0]] + ',' +'"' +  self.cloth_change[listofcharac[0][1]] + '_' + self.apperence_change[listofcharac[0][2]] + '"' + ','+ 'pos_c' + ')');
+            		line = self.original.readline()
+            		continue
             # 以下为对话和旁白
             if re.match('旁白', line):
                 span = re.match('旁白', line).span()
